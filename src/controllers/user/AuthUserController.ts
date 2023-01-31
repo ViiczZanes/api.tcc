16f19307f304891e0e24b3c5c77b115fab37de56
Victor Zanesco <victor.zanesco@outlook.com>
2023-01-31 02:48:39 -0300

    create all models

--- src/controllers/user/AuthUserController.ts
+++ src/controllers/user/AuthUserController.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express";
 import { AuthUserService } from "../../services/user/AuthUserService";
 
-
 const AuthUserController = {
   async handle(req: Request, res: Response) {
     const { email, password } = req.body;
+
 
     const response = await AuthUserService.execute({
       email,
